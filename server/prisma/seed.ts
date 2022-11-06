30205f9353fe9fc31954a28cc9b6e4d7aefac108
webster ribeiro <webster96ribeiro@gmail.com>
2022-11-06 20:21:53 -0300

    first commit

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient }  from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main(){
+    const user = await prisma.user.create({
+        data: {
+            name: 'Webs',
+            email: 'demo@gmail.com',
+            avatarUrl: 'https://github.com/webster9980.png',
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Exemple pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-02T12:00:00.690Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+    await prisma.game.create({
+        data: {
+            date: '2022-11-03T12:00:00.690Z',
+            firstTeamCountryCode: 'AR',
+            secondTeamCountryCode: 'BR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
